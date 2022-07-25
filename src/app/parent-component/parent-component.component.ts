985cf6c099b3abf396d2a407cd4e567e895dc131
Chris Goodkind <cgood97@vt.edu>
2022-07-25 11:47:35 -0400

    parent component comments

--- src/app/parent-component/parent-component.component.ts
+++ src/app/parent-component/parent-component.component.ts
@@ -7,11 +7,12 @@ import { SampleService } from '../sample-service.service';
   styleUrls: ['./parent-component.component.scss']
 })
 export class ParentComponentComponent {
-  
+
   // Anytime you need to use a service, you should declare it in the constructor and Angular will handle the rest
   // Once a service is injected/dependecy added, the component becomes "intelligent"
   constructor(private _sampleService: SampleService) {}
 
+  // Reacts to child component's updateEvent stream
   updateEvent(event: string) {
     console.log(event);
     console.log(this._sampleService.getSampleProperty());
